41772043793b5b62a6f885249a8d9c0d73b76f9d
Andrei7878 <99294222+Andrei7878@users.noreply.github.com>
2022-07-13 17:35:17 +0300

    fixing Task-5

---
+++ Homework-3/Задача №5/heroes.py
@@ -0,0 +1,66 @@
+class LasersHit:
+    def incinerate_with_lasers(self):
+        print('Wzzzuuuup!')
+
+class GunHit:
+    def fire_a_gun(self):
+        print('PIU PIU')
+
+class RoundHouseKickHit:
+    def roundhouse_kick(self):
+        print('Bump')
+
+class SuperHero:
+    def __init__(self, name: str, can_use_ultimate_attack: bool = True):
+        self._name = name
+        self._can_use_ultimate_attack = can_use_ultimate_attack
+
+    def find(self, place):
+        place.get_antagonist()
+
+    def attack(self):
+        print('Hero attacks!')
+
+    def ultimate(self):
+        if self.can_use_ultimate_attack:
+            print('Baaaaaam!')
+        else:
+            print('I need mana!')
+
+    @property
+    def can_use_ultimate_attack(self):
+        return self._can_use_ultimate_attack
+
+    @can_use_ultimate_attack.setter
+    def can_use_ultimate_attack(self, value: bool):
+        self._can_use_ultimate_attack = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+class ChuckNorris(SuperHero, GunHit, RoundHouseKickHit):
+    def __init__(self):
+        super().__init__('Chuck Norris')
+    def attack(self):
+        self.fire_a_gun()
+    def ultimate(self):
+        if self._can_use_ultimate_attack:
+            self.roundhouse_kick()
+        else:
+            print("Recharged!")
+
+class Superman(SuperHero, LasersHit):
+    def __init__(self):
+        super().__init__('Clark Kent')
+    def attack(self):
+        print("Kick!")
+    def ultimate(self):
+        if self._can_use_ultimate_attack:
+            self.incinerate_with_lasers()
+        else:
+            print("Recharged!")
